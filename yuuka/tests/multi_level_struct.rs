1a3f0e208dc64177896701cf9217b8f125850585
伊欧 <langyo.china@gmail.com>
2024-10-01 05:48:40 +0800

    🚧 Complete the single level braces' parse.

--- yuuka/tests/multi_level_struct.rs
+++ yuuka/tests/multi_level_struct.rs
@@ -1,5 +1,29 @@
 #[cfg(test)]
 mod test {
+    use yuuka::derive_config;
+
     #[test]
-    fn multi_level_struct() {}
+    fn multi_level_struct() {
+        derive_config!(Root {
+            a: String,
+            b: i32,
+            c: C {
+                d: f64,
+                e: E {
+                    f: bool,
+                },
+            },
+        });
+
+        let _ = Root {
+            a: "Hello".to_string(),
+            b: 42,
+            c: {
+                d: 3.14,
+                e: {
+                    f: true,
+                },
+            },
+        };
+    }
 }
