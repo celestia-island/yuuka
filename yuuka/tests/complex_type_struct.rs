ff6082173728e513346268431da82a76afaea816
伊欧 <langyo.china@gmail.com>
2024-10-01 09:21:23 +0800

    ⚡️ Complete complex type parse logic.

---
+++ yuuka/tests/complex_type_struct.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod test {
+    use yuuka::derive_config;
+
+    #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+    struct C {
+        d: f64,
+    }
+
+    #[test]
+    fn complex_type_struct() {
+        derive_config!(Root {
+            a: String,
+            b: i32,
+            c: super::C,
+        });
+
+        // let _ = Root {
+        //     a: "Hello".to_string(),
+        //     b: 42,
+        //     c: {
+        //         d: 3.14,
+        //         e: {
+        //             f: true,
+        //         },
+        //     },
+        // };
+    }
+}
