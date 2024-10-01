9e6ec7a83a97b2be1ee0a2b5db18a2731bac114d
伊欧 <langyo.china@gmail.com>
2024-10-01 09:30:41 +0800

    ✨ Add structs' generator.

--- yuuka/tests/single_level_struct.rs
+++ yuuka/tests/single_level_struct.rs
@@ -6,9 +6,9 @@ mod test {
     fn single_level_struct() {
         derive_config!(Root { a: String, b: i32 });
 
-        // let _ = Root {
-        //     a: "Hello".to_string(),
-        //     b: 42,
-        // };
+        let _ = Root {
+            a: "Hello".to_string(),
+            b: 42,
+        };
     }
 }
