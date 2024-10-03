9f97ca59aec8809c1dd567736daea31924252366
伊欧 <langyo.china@gmail.com>
2024-10-04 07:16:50 +0800

    🍻 Use default value assignment to generate default value.

---
+++ yuuka/tests/default_struct.rs
@@ -0,0 +1,49 @@
+#[cfg(test)]
+mod test {
+    use yuuka::derive_struct;
+
+    #[test]
+    fn empty_default_array() {
+        derive_struct!(Root {
+            a: [{
+                b: String = "hello".to_string()
+            }]
+        });
+
+        let val = Root::default();
+        assert_eq!(val.a.len(), 0);
+    }
+
+    #[test]
+    fn default_array() {
+        derive_struct!(Root {
+            a: [{
+                b: String = "hello".to_string()
+            }] = vec![{
+                b: "world".to_string()
+            }]
+        });
+
+        let mut val = Root::default();
+        assert_eq!(val.a.len(), 1);
+        assert_eq!(val.a[0].b, "world");
+        val.a.push(Default::default());
+        assert_eq!(val.a.len(), 2);
+        assert_eq!(val.a[1].b, "hello");
+    }
+
+    #[test]
+    fn default_enum() {
+        derive_struct!(Root {
+            a: enum Member {
+                Momoi,
+                Midori,
+                Yuzu,
+                Arisu,
+            } = Midori
+        });
+
+        let val = Root::default();
+        assert_eq!(val.a, Member::Midori);
+    }
+}
