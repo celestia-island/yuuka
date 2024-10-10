568081baf191bd94ba3f4d0c30d87a8b6b7e608b
伊欧 <langyo.china@gmail.com>
2024-10-10 20:26:19 +0800

    ✨ Now supports setting macros for specific keys.

--- yuuka/tests/extra_derive_struct.rs
+++ yuuka/tests/extra_derive_struct.rs
@@ -23,6 +23,27 @@ mod test {
     }
 
     #[test]
+    fn extra_derive_enum() {
+        derive_struct!(
+            #[serde(rename_all = "snake_case")]
+            Root {
+                nick_name: enum {
+                    SaibaMomoi,
+                    SaibaMidori,
+                    HanaokaYuzu,
+                    TendouAris,
+                } = SaibaMidori
+            }
+        );
+
+        let ret = Root::default();
+        assert_eq!(
+            serde_json::to_string(&ret).unwrap(),
+            r#"{"nick_name":"saiba_midori"}"#
+        );
+    }
+
+    #[test]
     fn extra_derive_struct_with_multi_level() {
         derive_struct!(
             #[serde(rename_all = "camelCase")]
@@ -61,6 +82,49 @@ mod test {
         assert_eq!(
             serde_json::to_string(&ret).unwrap(),
             r#"{"nickName":{"chinese":{"simplifiedChinese":{"firstName":{"origin":"早濑","meme":"旱濑"},"lastName":"优香"},"traditionalChinese":{"firstName":"早瀨","lastName":"優香"}},"japanese":{"firstName":"早瀬","lastName":"ユウカ"},"korean":{"firstName":"하야세","lastName":"유우카"},"english":{"firstName":"Hayase","lastName":"Yuuka"}}}"#
+        );
+    }
+
+    #[test]
+    fn extra_derive_struct_for_keys() {
+        derive_struct!(
+            #[serde(rename_all = "camelCase")]
+            Root {
+                nick_name: String,
+                #[serde(rename = "snack_case")]
+                live_in: String,
+            }
+        );
+
+        let ret = Root {
+            nick_name: "langyo".to_string(),
+            live_in: "China".to_string(),
+        };
+        assert_eq!(
+            serde_json::to_string(&ret).unwrap(),
+            r#"{"nickName":"langyo","live_in":"China"}"#
+        );
+    }
+
+    #[test]
+    fn extra_derive_enum_for_keys() {
+        derive_struct!(
+            #[serde(rename_all = "snake_case")]
+            Root {
+                nick_name: enum {
+                    SaibaMomoi,
+                    SaibaMidori,
+                    #[serde(rename = "yuzu")]
+                    HanaokaYuzu,
+                    TendouAris,
+                } = HanaokaYuzu
+            }
+        );
+
+        let ret = Root::default();
+        assert_eq!(
+            serde_json::to_string(&ret).unwrap(),
+            r#"{"nick_name":"yuzu"}"#
         );
     }
 }
