46126afd7965d088b85bdd83cb75d87405fdef8e
伊欧 <langyo.china@gmail.com>
2024-10-10 16:51:26 +0800

    ✨ Substructures can now also apply additional macros.

--- yuuka/tests/extra_derive_struct.rs
+++ yuuka/tests/extra_derive_struct.rs
@@ -21,4 +21,46 @@ mod test {
             r#"{"nickName":"langyo","liveIn":"China"}"#
         );
     }
+
+    #[test]
+    fn extra_derive_struct_with_multi_level() {
+        derive_struct!(
+            #[serde(rename_all = "camelCase")]
+            Root {
+                nick_name: {
+                    chinese: {
+                        simplified_chinese: {
+                            first_name: {
+                                origin: String = "早濑".to_string(),
+                                meme: String = "旱濑".to_string(),
+                            },
+                            last_name: String = "优香".to_string(),
+                        },
+                        traditional_chinese: {
+                            first_name: String = "早瀨".to_string(),
+                            last_name: String = "優香".to_string(),
+                        },
+                    }
+                    japanese: {
+                        first_name: String = "早瀬".to_string(),
+                        last_name: String = "ユウカ".to_string(),
+                    },
+                    korean: {
+                        first_name: String = "하야세".to_string(),
+                        last_name: String = "유우카".to_string(),
+                    },
+                    english: {
+                        first_name: String = "Hayase".to_string(),
+                        last_name: String = "Yuuka".to_string(),
+                    }
+                },
+            }
+        );
+
+        let ret = Root::default();
+        assert_eq!(
+            serde_json::to_string(&ret).unwrap(),
+            r#"{"nickName":{"chinese":{"simplifiedChinese":{"firstName":{"origin":"早濑","meme":"旱濑"},"lastName":"优香"},"traditionalChinese":{"firstName":"早瀨","lastName":"優香"}},"japanese":{"firstName":"早瀬","lastName":"ユウカ"},"korean":{"firstName":"하야세","lastName":"유우카"},"english":{"firstName":"Hayase","lastName":"Yuuka"}}}"#
+        );
+    }
 }
