a61b045843b767a5bd1d11ff377615c71a274e43
伊欧 <langyo.china@gmail.com>
2024-10-10 16:51:26 +0800

    ✨ Substructures can now also apply additional macros.

--- yuuka-macros/src/utils/derive_struct.rs
+++ yuuka-macros/src/utils/derive_struct.rs
@@ -28,6 +28,12 @@ impl DeriveStruct {
         *id.borrow_mut() += 1;
         ret
     }
+
+    pub fn extend_extra_macros(&self, extra_macros: ExtraMacros) -> Self {
+        let mut ret = self.clone();
+        ret.extra_macros.extend(extra_macros);
+        ret
+    }
 }
 
 impl Parse for DeriveStruct {
