496b4249ff75a1bce2f2f12d418c4e126a690e2f
伊欧 <langyo.china@gmail.com>
2024-11-30 03:09:40 +0800

    🎨 Remove `#[cfg(test)]`.

--- tests/multi_level_struct.rs
+++ tests/multi_level_struct.rs
@@ -1,25 +1,22 @@
-#[cfg(test)]
-mod test {
-    use yuuka::derive_struct;
+use yuuka::derive_struct;
 
-    #[test]
-    fn multi_level_struct() {
-        derive_struct!(Root {
-            a: String,
-            b: i32,
-            c: C {
-                d: f64,
-                e: E { f: bool },
-            },
-        });
+#[test]
+fn multi_level_struct() {
+    derive_struct!(Root {
+        a: String,
+        b: i32,
+        c: C {
+            d: f64,
+            e: E { f: bool },
+        },
+    });
 
-        let _ = Root {
-            a: "Hello".to_string(),
-            b: 42,
-            c: C {
-                d: std::f64::consts::PI,
-                e: E { f: true },
-            },
-        };
-    }
+    let _ = Root {
+        a: "Hello".to_string(),
+        b: 42,
+        c: C {
+            d: std::f64::consts::PI,
+            e: E { f: true },
+        },
+    };
 }
--- tests/visibility_test.rs
+++ tests/visibility_test.rs
@@ -1,39 +1,36 @@
-#[cfg(test)]
-mod test {
-    use yuuka::{derive_enum, derive_struct};
+use yuuka::{derive_enum, derive_struct};
 
-    #[derive(Debug, Clone, PartialEq, Default)]
-    struct C {
-        d: f64,
+#[derive(Debug, Clone, PartialEq, Default)]
+struct C {
+    d: f64,
+}
+
+derive_struct!(pub Root {
+    a_b: String,
+    B: i32,
+    c: C,
+});
+
+#[test]
+fn pub_type_struct() {
+    let _ = Root {
+        a_b: "Hello".to_string(),
+        B: 42,
+        c: C {
+            d: std::f64::consts::PI,
+        },
+    };
+}
+
+derive_enum!(
+    pub enum Root2 {
+        A,
+        B,
+        C,
     }
+);
 
-    derive_struct!(pub Root {
-        a_b: String,
-        B: i32,
-        c: C,
-    });
-
-    #[test]
-    fn pub_type_struct() {
-        let _ = Root {
-            a_b: "Hello".to_string(),
-            B: 42,
-            c: C {
-                d: std::f64::consts::PI,
-            },
-        };
-    }
-
-    derive_enum!(
-        pub enum Root2 {
-            A,
-            B,
-            C,
-        }
-    );
-
-    #[test]
-    fn pub_type_enum() {
-        let _ = Root2::A;
-    }
+#[test]
+fn pub_type_enum() {
+    let _ = Root2::A;
 }
