69c0aeb524c893680daa473040f78053940e0c9d
伊欧 <langyo.china@gmail.com>
2025-01-07 17:52:18 +0800

    ✨ Static type path can use `?:` spliter now.

--- tests/option_type_struct.rs
+++ tests/option_type_struct.rs
@@ -1,6 +1,17 @@
 use yuuka::derive_struct;
 
 #[test]
+fn option_type_basic() {
+    derive_struct!(Root {
+        a?: String
+    });
+
+    let _ = Root {
+        a: Some("hello".to_string()),
+    };
+}
+
+#[test]
 fn option_type_struct() {
     derive_struct!(Root {
         a?: A { b: String }
@@ -42,6 +53,24 @@ fn option_type_struct_anonymous() {
     let _ = Root {
         a: Some(__Root::_Root_0_anonymous {
             b: "hello".to_string(),
+        }),
+    };
+}
+
+#[test]
+fn option_type_enum_with_keys() {
+    derive_struct!(Root {
+        a?: enum AttackType {
+            Momoi,
+            Midori { b?: String },
+            Yuzu,
+            Arisu,
+        },
+    });
+
+    let _ = Root {
+        a: Some(AttackType::Midori {
+            b: Some("hello".to_string()),
         }),
     };
 }
